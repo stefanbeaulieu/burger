f3dbe8a8ca790e4be4c39fbd6e7c96b25d2c6ca4
Stefan Beaulieu <beaulieustefan@gmail.com>
2017-01-30 23:42:32 -0500

    Burger App

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/burgers/create", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function() {
+        res.redirect("/burgers");
+    });
+});
+
+router.put("/burgers/update/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function() {
+        res.redirect("/burgers");
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
